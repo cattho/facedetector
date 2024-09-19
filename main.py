c6ff5a3656e43bff2192531abf9b1c8f54a9a9f4
Sebastian Giraldo <jusegiraldo76@gmail.com>
2024-09-19 15:12:34 -0500

    perf: :boom: Code Modularization

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import cv2
 
-# Cargar el clasificador de Haar para la detección de rostros
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-# Capturar el video desde la cámara predeterminada (índice 0)
-cam = cv2.VideoCapture(1)
+# cargar el clasificador de Haar
+def load_face_cascade():
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    if face_cascade.empty():
+        print("Error: No se pudo cargar el clasificador de rostros")
+        exit
+    return face_cascade
 
-while True:
-    # Leer el cuadro de la cámara
-    _, img = cam.read()
 
-    # Convertir la imagen a escala de grises
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# inicializar camara
+def initialize_cam(index=0):
+    cam = cv2.VideoCapture(index)
+    if not cam.isOpened():
+        print("Error:No se pudo acceder a la cámara")
+        exit()
+    return cam
 
-    # Detectar rostros en la imagen en escala de grises
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    # Dibujar un rectángulo alrededor de cada rostro detectado
+# detectar rostros
+def detect_faces(face_cascade, gray_image):
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=4)
+    return faces
+
+
+# dibujo de rectangulos en los rostros
+def draw_faces(image, faces):
     for x, y, w, h in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    # Mostrar la imagen con los rectángulos dibujados
-    cv2.imshow("img", img)
 
-    # Esperar 30 ms y salir si se presiona la tecla 'ESC'
-    exit_program = cv2.waitKey(30)
-    if exit_program == 27:
-        break
+# main function del programa
+def main():
+    face_cascade = load_face_cascade()
+    cam = initialize_cam(0)
+    try:
+        while True:
+            ret, img = cam.read()
+            if not ret:
+                print("Error en el recuadro de la camara")
+                break
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detect_faces(face_cascade, gray)
+            draw_faces(img, faces)
+            cv2.imshow("Face Detection", img)
+            if cv2.waitKey(30) & 0xFF == 27:
+                break
+    except Exception as e:
+        print(f"error:{e}")
+    finally:
+        cam.release()
+        cv2.destroyAllWindows()
 
-# Liberar el objeto de captura y cerrar todas las ventanas
-cam.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
